0c2a7bc941dc63f3ffcec53859510bba2760f34a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2022-11-04 04:20:09 +0300

    add reducer combiner

--- src/assets/components/ReduxExample/ReduxExample.tsx
+++ src/assets/components/ReduxExample/ReduxExample.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Layout from '../Layout/Layout';
 import styles from './ReduxExample.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,7 +6,9 @@ import { useDispatch, useSelector } from 'react-redux';
 const ReduxExample = () => {
   const dispatch = useDispatch();
   // @ts-ignore
-  const statement1 = useSelector((state) => state.statement1);
+  const statement1 = useSelector((state) => state.first.statement1);
+  // @ts-ignore
+  const testArray = useSelector((state) => state.second.sampleArray);
 
   const decrementValue = () => {
     dispatch({ type: 'DECREMENT_STATE', payload: 10 });
@@ -15,9 +17,20 @@ const ReduxExample = () => {
     dispatch({ type: 'INCREMENT_STATE', payload: 10 });
   };
 
+  const textInput = useRef(null);
+
+  const addText = () => {
+    // @ts-ignore
+    dispatch({ type: 'ADD_ITEM', payload: textInput.current.value });
+  };
+
+  const clearText = () => {
+    dispatch({ type: 'CLEAR_ALL_ITEMS', payload: 0 });
+  };
+
   return (
     <Layout>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', margin: '10px 0' }}>
         <button className={styles.example} onClick={() => decrementValue()}>
           DECREMENT
         </button>
@@ -27,6 +40,23 @@ const ReduxExample = () => {
       </div>
 
       <div style={{ fontSize: '3rem' }}>Current count: {statement1}</div>
+
+      <div style={{ display: 'flex', margin: '10px 0' }}>
+        <button className={styles.example} onClick={() => addText()}>
+          Add text
+        </button>
+        <button className={styles.example} onClick={() => clearText()}>
+          Clear all text
+        </button>
+        <input type={'text'} ref={textInput} />
+      </div>
+
+      <div>
+        Current text:{' '}
+        {testArray.map((e: any, index: number) => (
+          <p key={index}>{e}</p>
+        ))}
+      </div>
     </Layout>
   );
 };
